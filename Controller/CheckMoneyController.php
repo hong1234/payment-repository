fac552f67e557e10f2fe86968b0b31f24bd97262
hong <hong@hong-510-p100ng.net>
2019-01-02 00:03:18 +0100

    Initial commit of payment module

---
+++ Controller/CheckMoneyController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hong1234
+ * Date: 04.11.16
+ * Time: 13:42
+ */
+
+namespace Foggyline\PaymentBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CheckMoneyController extends Controller
+{
+    public function authorizeAction(Request $request)
+    {
+        // Imagine we are calling the real payment processor API here, and getting some transaction id back
+        $transaction = md5(time() . uniqid()); // Just a dummy string, simulating some transaction id, if any
+
+        return new JsonResponse(array(
+            'success' => $transaction
+        ));
+    }
+
+    public function captureAction(Request $request)
+    {
+        // Imagine we are calling the real payment processor API here, and getting some transaction id back
+        $transaction = md5(time() . uniqid()); // Just a dummy string, simulating some transaction id, if any
+
+        return new JsonResponse(array(
+            'success' => $transaction
+        ));
+    }
+
+    public function cancelAction(Request $request)
+    {
+        // Imagine we are calling the real payment processor API here, and getting some transaction id back
+        $transaction = md5(time() . uniqid()); // Just a dummy string, simulating some transaction id, if any
+
+        return new JsonResponse(array(
+            'success' => $transaction
+        ));
+    }
+}
